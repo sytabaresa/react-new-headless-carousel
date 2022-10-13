8c35f3c5b45272d62d8cf459a2b2811fd2894845
Sebastian Tabares <sytabaresa@gmail.com>
2022-10-12 22:28:03 -0500

    prepare for package

--- src/carousel/infiniteCarousel.story.tsx
+++ src/carousel/infiniteCarousel.story.tsx
@@ -8,50 +8,72 @@ import {
 import { useState, useEffect, JSXInternal } from 'react';
 import { useCarousel } from './useCarousel';
 import './carousel.css'
+import { range } from '../utils';
+import { ExampleElement } from './common';
 
 export const __pageMeta: PageMeta = {
     title: 'Infinite Carousel',
-    description: 'My awesome infinite carousel.',
+    description: 'My awesome carousel.',
 };
 
 function CarouselStory() {
-    const { handlers, current, scrollTo, scrollNext, scrollPrev, useInfinite } = useCarousel()
+    // const { handlers, current, scrollTo, scrollNext, scrollPrev, useInfinite } = useCarousel()
     const [_current, setCurrent] = useState(0)
 
+    const { handlers, scrollNext, scrollPrev, scrollTo, current, useInfinite } = useCarousel()
 
     useEffect(() => {
         scrollTo(_current)
     }, [_current])
 
-    const ExampleElement = ({ n }: { n: number }) => (
-        <div className="w-full h-full mx-0 flex items-center justify-center bg-white">
-            <h1 className="block text-blue-400 text-2xl font-bold" >{n}</h1>
+    const items = range(5).map(i =>
+        <div className="carousel-item w-full h-40 m-2">
+            <ExampleElement n={i + 1} />
         </div>
     )
 
-    const generator = () =>
-    <div className="carousel-item w-full h-40">
-                            <ExampleElement n={i + 1} />
-                        </div>
-    const range = (i: number) => [...Array(i).keys()]
+    const slides = useInfinite(items)
+
 
     return (
         <>
             <Variant name="Single Item" description="Single Item carousel">
-                <div
-                    className="carousel w-40"
-                    {...handlers}
-                >
-                    
+                <div className="flex flex-col items-center">
+                    <div className="flex items-center justify-center">
+                        <div
+                            className="font-bold bg-white opacity-50 rounded-full h-6 w-6 text-lg text-black flex justify-center items-center m-2"
+                            onClick={() => scrollPrev()}
+                        >{"<"}</div>
+                        <div
+                            className="carousel w-40"
+                            {...handlers}
+                        >
+                            {/* {items} */}
+                            {slides}
+                        </div>
+                        <div
+                            className="font-bold bg-white opacity-50 rounded-full h-6 w-6 text-lg text-black flex justify-center items-center m-2"
+                            onClick={() => scrollNext()}
+                        >{">"}</div>
+                    </div>
+                    <div className="flex">
+                        {range(items.length - 1).map(i =>
+                            <div
+                                className={`w-2 h-2 mx-1 rounded-full  bg-white ${current == i ? '' : 'opacity-50'}`}
+                                onClick={() => scrollTo(i)}
+                            >
+                            </div>
+                        )}
+
+                    </div>
                 </div>
             </Variant>
-           
             <ControlsAddon>
                 <label>
                     Current
                     <input
                         type="number"
-                        value={_current}
+                        // value={current}
                         onChange={(e) => setCurrent(e.target.value)}
                     />
                 </label>
